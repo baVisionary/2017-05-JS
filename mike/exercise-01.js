01315adb922f0c4ac138b611da07b27dcf433e14
Mike <michael.a.aguilera@gmail.com>
2017-05-15 14:15:39 -0700

    updated the file again

--- mike/exercise-01.js
+++ mike/exercise-01.js
@@ -1,6 +1,7 @@
 // Create a function that takes two number parameters and logs the value of both
 // parameters added together.
 
-function somethingHere (1, 5){
-  console.log (1 + 5);
+function somethingHere(a, b){
+  console.log(a + b);
 }
+somethingHere(1,5);
