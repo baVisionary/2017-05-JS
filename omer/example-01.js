4111a87eca3a0df96905843871f3adaf745e0e19
Omer Syed <omers52195@gmail.com>
2017-05-15 11:19:58 -0700

    example_2

---
+++ omer/example-01.js
@@ -0,0 +1,26 @@
+//this is a comment
+
+/*Multiple line comment*/
+
+var num = 101;
+console.log(num);
+console.log(typeof num);
+
+var str = "hello";
+console.log(str);
+console.log(typeof str);
+
+
+var conditional = true;
+console.log(conditional);
+console.log(typeof conditional);
+
+
+var obj = console;
+console.log(obj);
+console.log(typeof obj);
+
+
+var func = console.log;
+console.log(func);
+console.log(typeof func);
---
+++ omer/example-02.js
@@ -0,0 +1,23 @@
+var num = 101;
+console.log(`num is ${num} : type is ${typeof num}`);
+
+
+var str = "hello";
+console.log(`str is ${str} : type is ${typeof str}`);
+
+
+
+var conditional = true;
+console.log(`conditional is ${conditional} : type is ${typeof conditional}`);
+
+
+
+
+var obj = console;
+console.log(`obj is ${obj} : type is ${typeof obj}`);
+
+
+
+
+var func = console.log;
+console.log(`func is ${func} : type is ${typeof func}`);
