5085b14dc5ec6762ebfd90971fb23035ecdfc069
Jason Tennant <xcodula@icloud.com>
2017-05-15 13:24:56 -0700

    exercise added

---
+++ jtennant/example-03.js
@@ -0,0 +1,8 @@
+// I am defining a function named 'test'
+function test() {
+    // Everything in between the curly brackets is the function body
+    console.log("hello");
+}
+
+// I am calling the function here.
+test();
