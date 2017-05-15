751aaf3322b9884e9fcaf131dabac7d8639157be
Jason Tennant <xcodula@icloud.com>
2017-05-15 11:13:23 -0700

    adding ex 1 and 2

---
+++ jtennant/example-02.js
@@ -0,0 +1,15 @@
+
+var num = 101;
+console.log(`num is ${num} : type is ${typeof num}`);
+
+var str = "hello";
+console.log(`num is ${str} : type is ${typeof str}`);
+
+var conditional = true;
+console.log(`num is ${conditional} : type is ${typeof conditional}`);
+
+var obj = console;
+console.log(`num is ${obj} : type is ${typeof obj}`);
+
+var func = console.log;
+console.log(`num is ${func} : type is ${typeof func}`);
