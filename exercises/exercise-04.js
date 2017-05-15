2c48e48d92686fd703cd5e6e801ffe6c7c0d92fe
Jason Tennant <xcodula@icloud.com>
2017-05-15 14:39:00 -0700

    exercise files added

---
+++ exercises/exercise-04.js
@@ -0,0 +1,6 @@
+// Create a function named 'strLength' that takes a string parameter and displays
+// the length of the string.
+
+
+strLength("123");   // should display 3
+strLength("1234");  // should display 4
---
+++ exercises/exercise-05.js
@@ -0,0 +1,12 @@
+// Create a function named 'max' that takes two number parameters and returns
+// the parameter with the largest value.
+
+
+var num1 = max(0,3);
+console.log(num1);  // should display 3
+
+var num2 = max(3,0);
+console.log(num2);  // should display 3
+
+var num3 = max(3,3);
+console.log(num3);  // should display 3
